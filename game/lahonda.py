c8dad2d72e681503dba8742dad482347ac542368
Vladimir Kremnev <kremniov98@gmail.com>
2019-10-03 10:13:27 +0200

    Modification de classe Map : ajout de variable grid, fonction draw_grid

--- game/lahonda.py
+++ game/lahonda.py
@@ -1,16 +1,24 @@
 #Le programme principal du jeu
-
+from classes.map import Map
 import pygame
 import time
+import os
 
 #Variables globales
 FPS = 60
 
 def main():
 	pygame.init()
+	
+	pygame.display.set_caption("Lahonda")
+
 	screen = pygame.display.set_mode((400,300))
 
 	clock = pygame.time.Clock()
+
+	map = Map(400, 300, 5)
+
+	map.draw_grid(screen)
 
 	done = False
 	#Boucle principale de jeu
@@ -29,9 +37,10 @@ def main():
 
 
 		pygame.display.flip()
-		
+	
+	pygame.quit()
 
 #Lancement du main
 if __name__ == "__main__":
 	main()
-	pygame.quit()
+	os.system("pause")
