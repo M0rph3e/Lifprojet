11292fcf627b168c564c80cb899a004f9b5b872f
Vladimir Kremnev <kremniov98@gmail.com>
2019-10-03 11:42:38 +0200

    Ajout de fonction dans Pawn : init, move, draw. Modification du main pour afficher le pion

--- game/lahonda.py
+++ game/lahonda.py
@@ -1,5 +1,7 @@
 #Le programme principal du jeu
 from classes.map import Map
+from classes.pawn import Pawn
+
 import pygame
 import time
 import os
@@ -18,16 +20,22 @@ def main():
 	screen = pygame.display.set_mode((HEIGHT, WIDTH))
 
 	clock = pygame.time.Clock()
+	pawn1 = Pawn(18,10)
+	pawn1.draw_pawn(screen, HEIGHT, WIDTH)
 
 	map = Map(20, 20)
-
 	map.draw_grid(screen)
+
+
 
 	done = False
 	#Boucle principale de jeu
 	while not done:
 		#Ceci permet de limiter le FPS dans jeu
 		clock.tick(FPS)
+		map.draw_grid(screen)
+		pawn1.draw_pawn(screen, 20, 20)
+		
 
 		#Gestion des evenements
 		for event in pygame.event.get():
@@ -37,6 +45,10 @@ def main():
 				if (event.type == pygame.KEYDOWN):
 					if event.key == pygame.K_ESCAPE:
 						done = True
+
+				if (event.type == pygame.KEYDOWN):
+					if event.key == pygame.K_f:
+						pawn1.move(15,10)
 
 
 		pygame.display.flip()
